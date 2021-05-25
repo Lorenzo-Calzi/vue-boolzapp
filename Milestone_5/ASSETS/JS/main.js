9acc9bea1f78e307f7dbca465c2ca9140af15f20
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-05-25 22:04:15 +0200

    Aggiunti Contatti

--- Milestone_5/ASSETS/JS/main.js
+++ Milestone_5/ASSETS/JS/main.js
@@ -77,19 +77,117 @@ const app = new Vue({
                 ],
             },
             {
-                name: 'Luisa',
+                name: 'Luigi',
                 avatar: './ASSETS/IMG/avatar_4.jpg',
                 visible: true,
                 messages: [
                     {
-                        date: '10/01/2020 15:30:55',
+                        date: '03/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
                     {
-                        date: '10/01/2020 15:50:00',
+                        date: '03/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
                         status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Alessio',
+                avatar: './ASSETS/IMG/avatar_5.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '15/02/2020 15:30:55',
+                        text: 'Come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15/02/2020 15:50:00',
+                        text: 'Tutto bene, te?',
+                        status: 'received'
+                    },
+                    {
+                        date: '15/02/2020 15:30:55',
+                        text: 'Bene, che fai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '15/02/2020 15:30:55',
+                        text: 'Sono occupato, ci sentiamo dopo!',
+                        status: 'received'
+                    },
+                ],
+            },
+            {
+                name: 'Chiara',
+                avatar: './ASSETS/IMG/avatar_io.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '04/05/2020 15:30:55',
+                        text: 'Eii Chiara',
+                        status: 'sent'
+                    },
+                    {
+                        date: '04/05/2020 15:50:00',
+                        text: 'Ciaoo, Dimmi tutto',
+                        status: 'received'
+                    },
+                    {
+                        date: '04/05/2020 15:52:25',
+                        text: 'A che ora passo?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '04/05/2020 15:58:10',
+                        text: 'Facciamo alle 9',
+                        status: 'received'
+                    },
+                    {
+                        date: '04/05/2020 15:52:25',
+                        text: '👍',
+                        status: 'sent'
+                    },
+                ],
+            },
+            {
+                name: 'Martina',
+                avatar: './ASSETS/IMG/avatar_6.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '19/11/2020 15:30:55',
+                        text: 'Ei marti, sei arrivata?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '19/11/2020 15:50:00',
+                        text: 'Sisi, tutto bene',
+                        status: 'received'
+                    }
+                ],
+            },
+            {
+                name: 'Edoardo',
+                avatar: './ASSETS/IMG/avatar_7.jpg',
+                visible: true,
+                messages: [
+                    {
+                        date: '23/06/2020 15:30:55',
+                        text: 'Ricordati di portare il pallone!',
+                        status: 'sent'
+                    },
+                    {
+                        date: '23/06/2020 15:50:00',
+                        text: 'Vai tranquillo 😎',
+                        status: 'received'
+                    },
+                    {
+                        date: '23/06/2020 15:30:55',
+                        text: 'Mi fido eh hahah',
+                        status: 'sent'
                     }
                 ],
             },
@@ -145,21 +243,14 @@ const app = new Vue({
             contattoAttivo.messages.splice(index, 1)
         },
 
-        /* removeContact(contatto_corrente){
-            console.log('Si' + contatto_corrente);
-            this.contacts.splice(contatto_corrente, 1)
-
-        } */
     }
 });
 
 
 /* Calcolo Data e Ora */
 var x = dayjs().format('DD/MM/YYYY');
-console.log(x);
 
 var y = dayjs().format('HH:mm:ss');
-console.log(y);
 
 document.getElementById('hour').innerHTML = y;
 document.getElementById('date').innerHTML = x;
